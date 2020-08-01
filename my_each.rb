68e7df043ff5f5294e838b8d6a8dc87c1821cdac
Hannah Angle <hannahcangle@gmail.com>
2020-08-01 06:07:25 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,3 @@
-def my_each # put argument(s) here
-  # code here
-end
-
-collection = [1, 2, 3, 4]
-my_each(collection) do |i|
-  puts i
-end
-
 collection = ("4", "5", "6")
 def my_each(array)
   i = 0 
