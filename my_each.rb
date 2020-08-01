23b7c982a3355c6c97522f3c937fee007c2d74cf
Hannah Angle <hannahcangle@gmail.com>
2020-08-01 06:05:36 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -6,3 +6,15 @@ collection = [1, 2, 3, 4]
 my_each(collection) do |i|
   puts i
 end
+
+collection = ("4", "5", "6")
+require 'pry'	def my_each(array)
+
+  i = 0 
+describe "my_each" do	  while i < array.length 
+  file = File.read('./my_each.rb')	  yield(array [i])
+
+  i = i + 1 
+  it "does not call on .each" do	end end
+    expect(file).to_not include(".each")	
+  end	  hello (["4", "5", "6"]) do |
